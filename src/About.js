53092085ac7db3fa23dac7d8857769cca0c7495e
Wayne A. Tipton <wayne.tipton@gmail.com>
2019-05-29 20:21:14 -0500

    first commit

---
+++ src/About.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export const About = () => (
+  <div>
+    <h2>About Page</h2>
+    <p>Why did anybody think the ending should be any different? All endings are not fairytales where everyone lives and things are all awesome. Why should Dany be any different then any other character? Her transition and breakdown went perfectly with how things were going in the story.</p>
+  </div>
+)
